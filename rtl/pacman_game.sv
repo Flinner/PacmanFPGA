66298767d18be122e30caf2b6bdcb994a7ae7270
Flinner Yuu <flinner@nand.sh>
2024-11-18 19:59:22 +0300

    feat: change to 4 bits per map location!

--- rtl/pacman_game.sv
+++ rtl/pacman_game.sv
@@ -154,14 +154,14 @@ module pacman_game #(
   ///////////////////////
   // MAP DRAWING LOGIC //
   ///////////////////////
-  logic [2:0] MAP[0:32*36-1];
+  logic [3:0] MAP[0:32*36-1];
   initial begin
     $display("Loading MAP from init file '%s'.", MAP_F);
     $readmemb(MAP_F, MAP);
   end
 
 
-  logic [2:0] map_location;
+  logic [3:0] map_location;
   //  STOP ANNOYING ME VERILATOR, I KNOW WHAT I WANT!!!
   /* verilator lint_off WIDTHEXPAND */
   assign map_location = MAP[(sx/8)+(sy/8)*32];
@@ -170,7 +170,7 @@ module pacman_game #(
   // TODO: remove useless check, since we check the screen on the RGB anyway
   always_comb begin
     // if (game_pix_stb) begin
-    R = {map_location, 1'b0} | R_PAC;  // TODO: change to 32!!
+    R = map_location | R_PAC;  // TODO: change to 32!!
     G = 4'h0 | G_PAC;
     B = 4'h0 | B_PAC;
     // end else begin

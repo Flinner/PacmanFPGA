c325514fdadd108c66d3ee4ba75ae4efaa77f579
Flinner Yuu <flinner@nand.sh>
2024-11-28 12:16:23 +0300

    feat: audio moved to BRAM

--- rtl/audio.sv
+++ rtl/audio.sv
@@ -26,10 +26,64 @@ module audio (
   logic [7:0] INTRO_AUDIO_DATA[0:INTRO_SAMPLES];  // 8-bit audio, 5736 samples
   logic [7:0] DEATH_AUDIO_DATA[0:DEATH_SAMPLES];  // 8-bit audio, 5736 samples
 
+  logic [7:0] chomp_byte;
+  logic [7:0] intro_byte;
+  logic [7:0] death_byte;
+
+
 
   initial $readmemh("mem/Chomp.mem", CHOMP_AUDIO_DATA);
   initial $readmemh("mem/Intro.mem", INTRO_AUDIO_DATA);
   initial $readmemh("mem/Death.mem", DEATH_AUDIO_DATA);
+
+  single_port_bram_with_rst #(  /*AUTOINSTPARAM*/
+      // Parameters
+      .DATA_WIDTH      (8),
+      .DATA_DEPTH      (CHOMP_SAMPLES),
+      .INITIAL_MEM_FILE("mem/Chomp.mem")
+  ) bram_chomp (  /*AUTOINST*/
+      // Outputs
+      .dout(chomp_byte),
+      // Inputs
+      .clk (clk_25MHZ),
+      .we  ('0),
+      .rst ('0),
+      .addr(address),
+      .di  ('0)
+  );
+
+  single_port_bram_with_rst #(  /*AUTOINSTPARAM*/
+      // Parameters
+      .DATA_WIDTH      (8),
+      .DATA_DEPTH      (INTRO_SAMPLES),
+      .INITIAL_MEM_FILE("mem/Intro.mem")
+  ) bram_intro (  /*AUTOINST*/
+      // Outputs
+      .dout(intro_byte),
+      // Inputs
+      .clk (clk_25MHZ),
+      .we  ('0),
+      .rst ('0),
+      .addr(address),
+      .di  ('0)
+  );
+
+  single_port_bram_with_rst #(  /*AUTOINSTPARAM*/
+      // Parameters
+      .DATA_WIDTH      (8),
+      .DATA_DEPTH      (DEATH_SAMPLES),
+      .INITIAL_MEM_FILE("mem/Death.mem")
+  ) brm_death (  /*AUTOINST*/
+      // Outputs
+      .dout(death_byte),
+      // Inputs
+      .clk (clk_25MHZ),
+      .we  ('0),
+      .rst ('0),
+      .addr(address),
+      .di  ('0)
+  );
+
 
   logic [13:0] address;  // Address counter for audio samples
   logic [ 7:0] current_sample;
@@ -37,28 +91,24 @@ module audio (
   always @(posedge clk_25MHZ) begin
     if (address >= AUDIO_SAMPLES) address <= 0;
     else address <= address + {12'b0, clk_8KHZ};
-    current_sample <= audio_data[address];
+    // current_sample <= audio_data[address];
   end
 
   always_comb
     case (sound_type)
       SOUND_LOADING: begin
-        AUDIO_SAMPLES = INTRO_SAMPLES;
-        audio_data[0:INTRO_SAMPLES] = INTRO_AUDIO_DATA;
+        current_sample = intro_byte;
       end
       SOUND_READY: begin
-        AUDIO_SAMPLES = INTRO_SAMPLES;
-        audio_data[0:INTRO_SAMPLES] = INTRO_AUDIO_DATA;
+        current_sample = intro_byte;
       end
       SOUND_WIN: begin
       end
       SOUND_GAME_PLAY: begin
-        AUDIO_SAMPLES = CHOMP_SAMPLES;
-        audio_data[0:CHOMP_SAMPLES] = CHOMP_AUDIO_DATA;
+        current_sample = chomp_byte;
       end
       SOUND_FAIL: begin
-        AUDIO_SAMPLES = DEATH_SAMPLES;
-        audio_data[0:DEATH_SAMPLES] = DEATH_AUDIO_DATA;
+        current_sample = death_byte;
       end
       default: ;
     endcase

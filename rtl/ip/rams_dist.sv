c5062f23dc431325ebae42150458f45522431c9f
Flinner Yuu <flinner@nand.sh>
2024-11-28 11:52:54 +0300

    feat: move sprite back!!!!!!!!!!!!!

--- rtl/ip/rams_dist.sv
+++ rtl/ip/rams_dist.sv
@@ -13,23 +13,22 @@ module rams_dist #(
     output logic [DATA_WIDTH-1:0] spo
 );
 
-  // generate
-  //   if (INITIAL_MEM_FILE != "NONE") begin : INITIALIZE_MEM_FROM_FILE
-  //     initial begin
-  //       $display("Loading MAP from init file '%s' to Distributed RAM.", INITIAL_MEM_FILE);
-  //       $readmemh(INITIAL_MEM_FILE, ram);
-  //     end
-  //   end else begin : DONT_INITIALIZE_MEM
-  //     initial begin
-  //       $display("Distributed RAM not initialized!");
-  //     end
-  //   end
-  // endgenerate
+  generate
+    if (INITIAL_MEM_FILE != "NONE") begin : INITIALIZE_MEM_FROM_FILE
+      initial begin
+        $display("Loading MAP from init file '%s' to Distributed RAM.", INITIAL_MEM_FILE);
+        $readmemh(INITIAL_MEM_FILE, ram);
+      end
+    end else begin : DONT_INITIALIZE_MEM
+      initial begin
+        $display("Distributed RAM not initialized!");
+      end
+    end
+  endgenerate
 
 
-  // logic [DATA_WIDTH-1:0] ram[0:DATA_DEPTH-1];
+  logic [DATA_WIDTH-1:0] ram[0:DATA_DEPTH-1];
 
-  // assign spo = ram[a];
-  assign spo = '1;
+  assign spo = ram[a];
 
 endmodule
